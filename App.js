aeb5b854b2644353e811ce0f777ff0a6b36cd477
AryakRoy <aryakroy63@gmail.com>
2021-03-04 10:34:29 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React ,{useState} from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from "./components/Task.js"
 export default function App() {
+  const [task,settask] = useState('');
+  const [tasks,settasks] = useState([]);
+  const handleAddTask = () => {
+    if (task !== ""){
+      Keyboard.dismiss();
+      settasks([...tasks,task]);
+      settask("");
+    } 
+  }
+  const completeTask = (index) => {
+    let itemsCopy = [...tasks];
+    itemsCopy.splice(index,1);
+    settasks(itemsCopy);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+        <View style={styles.items}>
+          {
+            tasks.map((item,index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task  text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+      <KeyboardAvoidingView 
+        behavior = {
+          Platform.OS === "ios" ? "padding" : "height"
+        }
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} value={task} onChangeText={(text) => settask(text)} placeholder="Add a Task"/>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +52,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper : {
+    paddingTop : 80,
+    paddingHorizontal : 20,
+
+  },
+  sectionTitle : {
+    fontSize : 24,
+    fontWeight : 'bold'
+  },
+  items : {
+    marginTop : 30
+  },
+  writeTaskWrapper : {
+    position : 'absolute',
+    bottom : 30,
+    width : '100%',
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+    alignItems : 'center'
+  },
+  input : {
+    backgroundColor : '#fff',
+    paddingVertical : 15,
+    paddingHorizontal : 25,
+    width : '70%',
+    borderRadius : 60,
+    borderColor : '#C0C0C0',
+  },
+  addWrapper : {
+    height : 60,
+    width : 60,
+    backgroundColor : '#fff',
+    borderRadius : 60,
+    justifyContent : 'center',
+    alignItems : 'center',
+    borderColor : '#C0C0C0',
+  },
+  addText : {
+    fontSize : 27,
+    color : '#C0C0C0'
   },
 });
